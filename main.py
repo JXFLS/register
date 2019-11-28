98a84a731d0e4e1c9dabba82226a2e223f7961a5
AlessandroChen <alex_passenger@outlook.com>
2019-11-28 12:44:13 +0800

    add mutlti name prefix feature

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ op = webdriver.Chrome()
 URL = 'http://172.16.254.82/selfLogon.do'
 num = '0000'
 wait = WebDriverWait(op, 10)
-name = "JXJJZX"
+name = ["JXJJZX", "JXJJXX"]
 valiable = list()
 
 def nxt(num):
@@ -44,11 +44,11 @@ def get_captcha():
     print("captcha = " + str(open("res.txt").read()))
     return open("res.txt").read()
 
-def login(num):
+def login(nam, num):
     tmp = 1
     while tmp < 10:
         try :
-            op.find_element_by_name("user_id").send_keys(name + num)
+            op.find_element_by_name("user_id").send_keys(nam + num)
             op.find_element_by_name("passwd").send_keys("123456")
             time.sleep(2)
             op.find_element_by_name("validateCode").send_keys(get_captcha())
@@ -61,13 +61,13 @@ def login(num):
             if tmp == 10:
                 break
 
-def work(num):
+def work(nam, num):
     try:
         op.get(URL)
     except:
         return 
-    print("work " + str(num))
-    login(num)
+    print("work " + nam + str(num))
+    login(nam, num)
     tmpqwq = 1
     not_ok = int()
     while 1:
@@ -75,7 +75,7 @@ def work(num):
             tmp = wait.until(EC.presence_of_element_located((By.NAME, "mainFrame")))
         except:
             print("wrong")
-            login(num)
+            login(nam, num)
         else:
             break
         finally:
@@ -100,18 +100,17 @@ def work(num):
     if ans < 15:
         global valiable
         valiable.append(num)
+        with open("result.txt", 'a') as out:
+            out.write(nam + str(n))
     
     print(str(num) + " = " + str(ans))
 
 if __name__ == '__main__':
 
-    while 1:
+    for nam in name:
         num = nxt(num) 
-        work(num)
+        work(nam, num)
         if int(num) > 240:
-            break
-    with open("result.txt", 'a') as out:
-        for n in valiable:
-            out.write(str(n))
+            continue
 
     op.quit()

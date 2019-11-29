9bd167f4601d204ff67712ce95078fc8409691e9
blackwhitetony <blackwhitetony@outlook.com>
2019-11-29 12:17:02 +0800

    说明

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ register.register('JXJJZX').mainFuc()
 """以 JXJJZX 为前缀遍历 0001 到 0240"""
 register.register('JXJJXX', 10, 200).mainFuc()
 """以 JXJJXX 为前缀遍历 0010 到 0200"""
-register.register('JXJJZX', 0, 0, 1).mainFuc()
-"""以 JXJJZX 为前缀遍历在 result.txt 中出现的帐号"""
+register.register('JXJJZX', 10, 200, 1).mainFuc()
+"""以 JXJJZX 为前缀遍历在 result.txt 中出现的且在 [10, 200] 中的帐号"""
